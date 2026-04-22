71243cc744086690e76615c9825d942f2a5dd60c
Cleber Fernandes <193790081+Corujitoo@users.noreply.github.com>
2026-04-22 21:54:37 +0100

    feat: add exerciciofinal/script.js

---
+++ exerciciofinal/script.js
@@ -0,0 +1,18 @@
+const button = document.getElementById("btn-treino");
+
+button.addEventListener("click", function() {
+   console.log("Botão clicado!");
+});
+
+const button2 = document.getElementById("btn-editar");
+
+button2.addEventListener("click", function() {
+    console.log("Botão de edição clicado!");
+});
+
+
+const button3 = document.getElementById("btn-delete");
+
+button3.addEventListener("click", function() {
+    console.log("Botão de deletar clicado!");
+});
